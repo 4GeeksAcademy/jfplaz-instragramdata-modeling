9a1deaefb05cbb2f1ecff4cf4bccdc6bcf2ce7d4
Jesus F Plaz <jfplaz@gmail.com>
2025-02-12 13:36:46 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -1,26 +1,91 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from sqlalchemy import create_engine, Column, Table, ForeignKey, Integer,String,Enum
 from eralchemy2 import render_er
+import enum
+from typing import List
+
+class MyEnum(enum.Enum):
+    video = ".mp4"
+    imagen= ".img"
+
+
+
+
+
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+# Segun la documentacion tenia que hacer una tabla de relacion. las hice pero no las entendi  y quede en blanco y por eso las
+# comentadas. si puedes dejame un feedback al respecto. 
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# post_association = Table(
+#     "post_association",
+#     Base.metadata,
+#     Column("user_id", ForeignKey("user.id"), primary_key=True),
+#     Column("post_id", ForeignKey("post.id"), primary_key=True),
+# )
+
+# Tabla de asociación para Comment
+# comment_association = Table(
+#     "comment_association",
+#     Base.metadata,
+#     Column("user_id", ForeignKey("user.id"), primary_key=True),
+#     Column("comment_id", ForeignKey("comment.id"), primary_key=True),
+# )
+
+follower = Table(
+    "followers",
+    Base.metadata,
+    Column("user_from_id", ForeignKey("user.id")),
+    Column("user_to_id", ForeignKey("user.id")),
+)
+
+class User(Base):
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
+    firtsname: Mapped[str] = mapped_column(nullable=False)
+    lastname: Mapped[str] = mapped_column(String(80),nullable=False, unique=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    posts: Mapped[List["Post"]] = relationship("Post", back_populates="author")
+    comments: Mapped[List["Comment"]] = relationship("Comment", back_populates="author")
+
+   
+class Comment(Base):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(nullable=False)
+    author_id: Mapped[int] =  mapped_column(ForeignKey('user.id'))
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey("post.id"), nullable=False)
+
+    # Relaciones
+    author: Mapped["User"] = relationship("User", back_populates="comments")
+    post: Mapped["Post"] = relationship("Post", back_populates="comments")
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    media: Mapped["media"] = relationship()
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
+
+    #  Relaciones
+    author: Mapped["User"] = relationship("User", back_populates="posts")
+    comments: Mapped[List["Comment"]] = relationship("Comment", back_populates="post")
+    media: Mapped[List["Media"]] = relationship("Media", back_populates="post")
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[MyEnum] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey("post.id"), nullable=False)
+
+    #  Relación con Post
+    post: Mapped["Post"] = relationship("Post", back_populates="media")
 
     def to_dict(self):
         return {}
